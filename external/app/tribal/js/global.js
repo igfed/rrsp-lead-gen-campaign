e103148910513f0e86cf6da634ef9a710522e7b6
Dennis Erny <ERNYDE1@ig.bz>
2017-01-10 14:51:04 -0500

    Fixed video

--- external/app/tribal/js/global.js
+++ external/app/tribal/js/global.js
@@ -2,67 +2,54 @@
 var ScrollMan = (function () {
   function ScrollMan() {
   }
-
   ScrollMan.to = function (e, d, t) {
-    if (d === void 0) {
-      d = 0;
-    }
-    if (t === void 0) {
-      t = 0.25;
-    }
+    if (d === void 0) { d = 0; }
+    if (t === void 0) { t = 0.25; }
     var el;
     var typ = typeof e;
     el = (typ === 'string') ? $(e) : e;
     if (el) {
       var ny = el.offset().top;
-      TweenLite.to(window, t, { delay: d, scrollTo: { y: ny }, autoKill: false });
+      TweenLite.to(window, t, { delay: d, scrollTo: { y: ny }, autoKill:false });
     }
   };
   ScrollMan.toNextElementFrom = function (el, d, t) {
-    if (d === void 0) {
-      d = 0;
-    }
-    if (t === void 0) {
-      t = 0.25;
-    }
+    if (d === void 0) { d = 0; }
+    if (t === void 0) { t = 0.25; }
     var ny = $(el.nextElementSibling).offset().top;
-    TweenLite.to(window, t, { delay: d, scrollTo: { y: ny }, autoKill: false });
+    TweenLite.to(window, t, { delay: d, scrollTo: { y: ny }, autoKill:false });
   };
   ScrollMan.toTop = function (d, t) {
-    if (d === void 0) {
-      d = 0;
-    }
-    if (t === void 0) {
-      t = 0.25;
-    }
-    TweenLite.to(window, t, { delay: d, scrollTo: { y: 0 }, autoKill: false });
+    if (d === void 0) { d = 0; }
+    if (t === void 0) { t = 0.25; }
+    TweenLite.to(window, t, { delay: d, scrollTo: { y: 0 }, autoKill:false });
   };
   return ScrollMan;
 })();
 
 // TRACKING
-var igTrack = (function () {
+var igTrack = (function(){
 
-  var trackingID = null;
+  var trackingID  = null;
   var DEFAULT = ['send', 'event'];
   var EVENTS = {
-    'SCROLL_MORE': ['Engage', 'Click'],
-    'SEARCH': ['Engage', 'Search'],
-    'GET_STARTED': ['Engage', 'Click'],
-    'TIP_DISMISS': ['Engage', 'Click'],
+    'SCROLL_MORE':      ['Engage', 'Click'],
+    'SEARCH':           ['Engage', 'Search'],
+    'GET_STARTED':      ['Engage', 'Click'],
+    'TIP_DISMISS':      ['Engage', 'Click'],
     'QUIZ_SUMMARY_CTA': ['Engage', 'Click'],
-    'QUIZ_START': ['Engage', 'Click', 'OS_Quiz_Start'],
-    'QUIZ_PROGRESS': ['Engage', 'Click'],
-    'QUIZ_ANSWER': ['Engage', 'Expand'],
-    'QUIZ_COMPLETED': ['Engage', 'View'],
-    'AREA_EXPAND': ['Engage', 'Expand'],
-    'REPORT_PRINT_DL': ['Consider', 'DownloadReport', 'DownloadReport'],
-    'REPORT_VIEW': ['Consider', 'View', 'ViewReport'],
-    'ADVISOR_SEARCH': ['Convert', 'Search'],
-    'VIDEO_PLAY': ['Engage', 'Play'],
+    'QUIZ_START':       ['Engage', 'Click', 'OS_Quiz_Start'],
+    'QUIZ_PROGRESS':    ['Engage', 'Click'],
+    'QUIZ_ANSWER':      ['Engage', 'Expand'],
+    'QUIZ_COMPLETED':    ['Engage', 'View'],
+    'AREA_EXPAND':      ['Engage', 'Expand'],
+    'REPORT_PRINT_DL':  ['Consider', 'DownloadReport', 'DownloadReport'],
+    'REPORT_VIEW':      ['Consider', 'View', 'ViewReport'],
+    'ADVISOR_SEARCH':   ['Convert', 'Search'],
+    'VIDEO_PLAY':      ['Engage', 'Play'],
   };
 
-  var TRACK = function (eventID) {
+  var TRACK = function(eventID) {
 
     var eventArgs = null;
 
@@ -74,7 +61,7 @@ var igTrack = (function () {
       switch (eventID) {
         case 'SEARCH' :
           eventArgs = eventArgs.concat(
-            'Search_Query?' + args[1]
+            'Search_Query?'+args[1]
           );
           break;
 
@@ -99,7 +86,7 @@ var igTrack = (function () {
 
         case 'AREA_EXPAND' :
           var areaId = args[1].split('-');
-          areaId.forEach(function (part, ix, arr) {
+          areaId.forEach(function(part, ix, arr){
             arr[ix] = part.charAt(0).toUpperCase() + part.slice(1);
           });
           areaId = areaId.join('_');
@@ -143,16 +130,20 @@ var igTrack = (function () {
     }
 
     if (window['ga'] && eventArgs) {
-      console.log('click', eventArgs)
+      console.log('click',eventArgs)
       TRACK.doGA(eventArgs);
     }
   };
 
-  TRACK.doGA = function (args) {
+  TRACK.doGA = function(args) {
     ga.apply(ga, args);
   };
+  // TRACK.init = function(id) {
+  //     trackingID = id;
+  //     //ga init
+  // };
 
-  TRACK.inline = function (element) {
+  TRACK.inline = function(element){
 
     var eventArgs = DEFAULT.concat(
       element.getAttribute('data-tracking-category') || 'Engage',
@@ -168,13 +159,11 @@ var igTrack = (function () {
       TRACK.doGA(eventArgs);
     }
   };
-
   return TRACK;
-
 })();
 
 // SEARCH BOX
-$('.searchbox button').click(function (e) {
+$('.searchbox button').click(function(e){
 
   var $parent = $(this).closest('.searchbox');
   var $input = $parent.find('.inputText');
@@ -184,13 +173,11 @@ $('.searchbox button').click(function (e) {
     e.preventDefault();
     $input.attr('placeholder', $input.attr('data-placeholder-default'));
     $parent.addClass('open');
-    if (!$.browser.msie) {
-      $parent.find('.inputText').focus();
-    }
+    if (!$.browser.msie) { $parent.find('.inputText').focus(); }
 
   } else {
 
-    if ($input.val() === '') {
+    if ($input.val() === ''){
       e.preventDefault();
       $input.attr('placeholder', $input.attr('data-placeholder-empty-msg'));
     } else {
@@ -199,28 +186,28 @@ $('.searchbox button').click(function (e) {
   }
 });
 
-$(document).on('click', function (e) {
-  $('.searchbox.open').each(function () {
+$(document).on('click', function(e) {
+  $('.searchbox.open').each(function(){
     if (!$.contains(this, e.target)) {
       $(this).removeClass('open');
     }
   });
 });
 
-// FOUNDATION INIT
-$(document).foundation({
-  accordion: {
-    multi_expand: true,
-    callback: function (accordion) {
-
-      var isAreasAcc = $.contains($('.areas.section')[0], accordion[0]);
-      if (isAreasAcc && $(accordion).hasClass('active')) {
-        var areaId = $(accordion).closest('.area').attr('data-areaId');
-        igTrack('AREA_EXPAND', areaId);
-      }
-    }
-  }
-});
+// // FOUNDATION INIT
+// $(document).foundation({
+//   accordion: {
+//     multi_expand: true,
+//     callback: function (accordion) {
+//
+//       var isAreasAcc = $.contains($('.areas.section')[0], accordion[0]);
+//       if (isAreasAcc && $(accordion).hasClass('active')) {
+//         var areaId = $(accordion).closest('.area').attr('data-areaId');
+//         igTrack('AREA_EXPAND', areaId);
+//       }
+//     }
+//   }
+// });
 
 if (
   ($.browser.msie === true) &&
@@ -242,7 +229,7 @@ function onTemplateLoad(experienceID) {
 function onTemplateReady(evt) {
   $('.video-spinner-container').hide();
   $('.js-video-play').show();
-  $('.js-video-play-btn').on('click', function () {
+  $('.js-video-play-btn').on('click',function(){
 
     event.preventDefault ? event.preventDefault() : (event.returnValue = false);
     $('.js-video-play').hide();
@@ -252,29 +239,30 @@ function onTemplateReady(evt) {
     videoPlayer.play();
   })
 
-  $(window).on("orientationchange", function (event) {
+  $( window ).on( "orientationchange", function( event ) {
     var resizeWidth = $(".BrightcoveExperience").width(),
       resizeHeight = $(".BrightcoveExperience").height();
-    if (experienceModule.experience.type == "html") {
+    if (experienceModule.experience.type == "html"){
       experienceModule.setSize(resizeWidth, resizeHeight)
     }
   });
 }
 
-$('.search-clsBtn').on('click', function () {
+$('.search-clsBtn').on('click',function(){
   $('search-input').val("");
 
 })
 //Code below is breaking mobile nav, don't know why
-$('a.nav-link').on('touchend', function () {
-  console.log('XXXX', $(this).attr('class'));
+$('a.nav-link').on('touchend', function() {
+  console.log('XXXX',$(this).attr('class'));
   var linkTest = $(this).attr('class');
 
   if (linkTest.indexOf('has-subnav')) {
     var link = $(this).attr('href');
-    window.open(link, '_parent'); // opens in new window as requested
+    window.open(link,'_parent'); // opens in new window as requested
     return false; // prevent anchor click
   }
+
 });
 
 if ($('html').attr('lang') == 'fr') {
@@ -282,7 +270,7 @@ if ($('html').attr('lang') == 'fr') {
   $('.accordion--subnav').css({
     "margin-left": 0,
     "width": "100%"
-  })
+  });
   //$('#panel1b ul.accordion--subnav li:last-child a').attr('href','https://bvi.bnc.ca/index/investorsgroup/indexfr.html')
 } else {
   $('.accordion--subnav').css({
@@ -291,5 +279,3 @@ if ($('html').attr('lang') == 'fr') {
   });
   //$('#panel1b ul.accordion--subnav li:last-child a').attr('href','https://bvi.bnc.ca/index/investorsgroup/indexen.html')
 }
-
-   
