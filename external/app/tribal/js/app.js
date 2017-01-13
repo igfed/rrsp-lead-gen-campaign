bba13cc7068254f991563227962b947f318f6b6b
Dennis Erny <ERNYDE1@ig.bz>
2017-01-13 17:02:55 -0500

    Removed stray console.log

--- external/app/tribal/js/app.js
+++ external/app/tribal/js/app.js
@@ -34,9 +34,6 @@
       if ($(".call-request-form").length) {
         setValidation();
       }
-      $('.js-video-play').on('click', function(){
-        console.log('video clicked');
-      });
     }
 
     function handleSubmit(event) {
